a24469bd4ac5c2551ae722c44f32ebbb3e7fb1ed
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-10-16 19:10:48 +0700

    1.0.5 port forge ecosystem again... it's bad being stupid

---
+++ forge/src/main/java/net/litetex/rpf/RabbitPathfindingFixed.java
@@ -0,0 +1,9 @@
+package net.litetex.rpf;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("rabbit_pathfinding_fix")
+public class RabbitPathfindingFixed {
+
+    public RabbitPathfindingFixed() {}
+}
