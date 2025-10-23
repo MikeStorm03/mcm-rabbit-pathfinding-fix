2cab40d3e7b743eb88ddee0f9f62cb0e53fc8fb9
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-10-23 09:29:44 +0700

    done neoforge but dev enviroment still dead af

---
+++ neoforge/src/main/java/net/litetex/rpf/RabbitPathfindingFixed.java
@@ -0,0 +1,9 @@
+package net.litetex.rpf;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("rabbit_pathfinding_fix")
+public class RabbitPathfindingFixed {
+
+    public RabbitPathfindingFixed() {}
+}
